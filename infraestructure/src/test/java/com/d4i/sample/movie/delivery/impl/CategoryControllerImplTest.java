cc190684635140bfd0a6c70996ac18349cb88ee9
Biz1999 <alessanbiz@gmail.com>
2022-11-20 17:23:16 -0300

    test: create category and category already exists

--- infraestructure/src/test/java/com/d4i/sample/movie/delivery/impl/CategoryControllerImplTest.java
+++ infraestructure/src/test/java/com/d4i/sample/movie/delivery/impl/CategoryControllerImplTest.java
@@ -1,6 +1,7 @@
 package com.d4i.sample.movie.delivery.impl;
 
 import com.d4i.sample.movie.category.Category;
+import com.d4i.sample.movie.category.exception.CategoryAlreadyExistException;
 import com.d4i.sample.movie.category.usecase.CreateCategoryUseCase;
 import com.d4i.sample.movie.category.usecase.GetAllCategoriesUseCase;
 import com.d4i.sample.movie.delivery.converters.CategoryRestConverter;
@@ -11,6 +12,7 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
@@ -21,8 +23,12 @@ import java.nio.file.Paths;
 import java.util.*;
 
 import static java.nio.file.Files.readString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -83,6 +89,46 @@ class CategoryControllerImplTest {
                 .getResponse()
                 .getContentAsString();
 
+        JSONAssert.assertEquals(expectedResponse, responseBody, true);
+    }
+
+    @Test
+    void should_create_a_category_successfully() throws Exception {
+        String expectedResponse = getExpectedResponse("create_category_200_response.json");
+
+        String responseBody = mockMvc.perform(post(RestConstants.APPLICATION_NAME + RestConstants.API_VERSION_1 + RestConstants.RESOURCE_CATEGORY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"Drama\",\"available\": \"true\"}"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JSONAssert.assertEquals(expectedResponse, responseBody, true);
+    }
+
+
+    @Test
+    void should_throw_an_exception_when_category_already_exists_but_answers_with_200_code() throws Exception {
+        String expectedResponse = getExpectedResponse("create_category_200_response.json");
+
+        CategoryRest dramaCategoryRest = new CategoryRest("Drama", true);
+        Category dramaCategory = new Category(null, "Drama", true);
+
+        when(categoryRestConverter.mapToEntity(dramaCategoryRest)).thenReturn(dramaCategory);
+        doThrow(new CategoryAlreadyExistException()).when(createCategoryUseCase).execute(any());
+
+        String responseBody = mockMvc.perform(post(RestConstants.APPLICATION_NAME + RestConstants.API_VERSION_1 + RestConstants.RESOURCE_CATEGORY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"Drama\",\"available\": \"true\"}"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertThrows(CategoryAlreadyExistException.class, () -> createCategoryUseCase.execute(any()));
         JSONAssert.assertEquals(expectedResponse, responseBody, true);
     }
 
