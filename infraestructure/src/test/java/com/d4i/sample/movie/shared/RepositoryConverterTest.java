fcf3a337c02d37f6701dcbe4b9008b103bab0639
Biz1999 <alessanbiz@gmail.com>
2022-11-21 01:19:36 -0300

    test: converters interface

--- infraestructure/src/test/java/com/d4i/sample/movie/shared/RepositoryConverterTest.java
+++ infraestructure/src/test/java/com/d4i/sample/movie/shared/RepositoryConverterTest.java
@@ -1,4 +1,41 @@
+package com.d4i.sample.movie.shared;
+
+import com.d4i.sample.movie.category.Category;
+import com.d4i.sample.movie.persistence.entities.CategoryEntity;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RepositoryConverterTest {
-  
+    RepositoryConverter repositoryConverter = new RepositoryConverter() {
+        @Override
+        public Serializable mapToTable(Serializable persistenceObject) {
+            return RepositoryConverter.super.mapToTable(persistenceObject);
+        }
+
+        @Override
+        public Serializable mapToEntity(Serializable tableObject) {
+            return RepositoryConverter.super.mapToEntity(tableObject);
+        }
+    };
+
+    @Test
+    void should_throw_unsupported_operation_exception_when_trying_to_map_to_table() {
+        Category dramaCategory = new Category(null, "Drama", false);
+
+        RuntimeException exception = assertThrows(UnsupportedOperationException.class, () -> repositoryConverter.mapToTable(dramaCategory));
+
+        assertNull(exception.getMessage());
+    }
+
+    @Test
+    void should_throw_unsupported_operation_exception_when_trying_to_map_to_entity() {
+        CategoryEntity categoryEntity = new CategoryEntity(Long.parseLong("0"), "Drama", false);
+
+        RuntimeException exception = assertThrows(UnsupportedOperationException.class, () -> repositoryConverter.mapToEntity(categoryEntity));
+
+        assertNull(exception.getMessage());
+    }
 }
--- infraestructure/src/test/java/com/d4i/sample/movie/shared/RestConverterTest.java
+++ infraestructure/src/test/java/com/d4i/sample/movie/shared/RestConverterTest.java
@@ -1,4 +1,44 @@
+package com.d4i.sample.movie.shared;
+
+import com.d4i.sample.movie.category.Category;
+import com.d4i.sample.movie.delivery.rest.CategoryRest;
+import com.d4i.sample.movie.persistence.entities.CategoryEntity;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RestConverterTest {
-  
+    RestConverter restConverter = new RestConverter() {
+
+        @Override
+        public Serializable mapToEntity(Serializable rest) {
+            return RestConverter.super.mapToEntity(rest);
+        }
+
+        @Override
+        public Serializable mapToRest(Serializable entity) {
+            return RestConverter.super.mapToRest(entity);
+        }
+    };
+
+    @Test
+    void should_throw_unsupported_operation_exception_when_trying_to_map_to_entity() {
+        CategoryRest dramaCategoryRest = new CategoryRest("Drama", true);
+
+        RuntimeException exception = assertThrows(UnsupportedOperationException.class, () -> restConverter.mapToEntity(dramaCategoryRest));
+
+        assertNull(exception.getMessage());
+    }
+
+    @Test
+    void should_throw_unsupported_operation_exception_when_trying_to_map_to_rest() {
+        Category category = new Category(Long.parseLong("0"), "Drama", false);
+
+        RuntimeException exception = assertThrows(UnsupportedOperationException.class, () -> restConverter.mapToRest(category));
+
+        assertNull(exception.getMessage());
+    }
+
 }
